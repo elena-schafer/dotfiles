378230346f9ff26d7d4e228c6a3406fe2babf993
Egan Schafer <eganschafer@gmail.com>
2023-10-12 15:18:57 -0600

    feat: keybinds for NvimTree, Bufferline & floaterm

--- nvim/lua/custom-keys.lua
+++ nvim/lua/custom-keys.lua
@@ -1,5 +1,14 @@
 local map = vim.api.nvim_set_keymap
 
+-- Nvim-Tree keybinds
 map('n', '<C-n>', [[:NvimTreeToggle<CR>]], {})
+
+--BufferLine keybinds
 map('n', '[b', [[:BufferLineCyclePrev<CR>]], {})
 map('n', ']b', [[:BufferLineCycleNext<CR>]], {})
+
+--floaterm keybinds
+map('n', '<F5>', [[:FloatermNew<CR>]], {})
+map('n', '<F6>', [[:FloatermToggle<CR>]], {})
+map('n', '<F7>', [[:FloatermNext<CR>]], {})
+map('n', '<F8>', [[:FloatermPrev<CR>]], {})
