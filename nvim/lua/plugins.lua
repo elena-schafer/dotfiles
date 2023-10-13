b158000645d24441fb10c2e468a82a539901e7d6
Egan Schafer <eganschafer@gmail.com>
2023-10-13 13:15:58 -0600

    feat: Added Noice plugin, pretty command line
    Added Noice plugin, which provides a pretty looking floating command
    line and floating messages for command results.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -34,4 +34,5 @@ require("lazy").setup({
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 	{ "nvim-telescope/telescope.nvim", tag = '0.1.4', dependencies = "nvim-lua/plenary.nvim"},
+	{ "folke/noice.nvim", event = "VeryLazy", dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify", } }
 })
