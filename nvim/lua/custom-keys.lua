ecfd534dec7f57cd094ab80cac10b9130b75c0ba
Egan Schafer <eganschafer@gmail.com>
2023-10-13 13:18:37 -0600

    feat: Added keybinds for Telescope plugin
    Added keybinds for accessing fuzzy search features of the Telescope
    plugin.

--- nvim/lua/custom-keys.lua
+++ nvim/lua/custom-keys.lua
@@ -12,3 +12,10 @@ map('n', '<leader>ft', [[:FloatermNew<CR>]], {})
 map('n', '<leader>t', [[:FloatermToggle<CR>]], {})
 --map('n', '<M->', [[:FloatermNext<CR>]], {})
 --map('n', '<M->', [[:FloatermPrev<CR>]], {})
+
+--Telescope keybinds
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
