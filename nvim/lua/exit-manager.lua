3e2c0d073841e1be86e6a53b6ced4560cf6a4b8b
Egan Schafer <eganschafer@gmail.com>
2024-01-25 15:32:04 -0700

    fix: improved exit functionality

--- nvim/lua/exit-manager.lua
+++ nvim/lua/exit-manager.lua
@@ -3,19 +3,15 @@ local function close_nvim_tree_on_exit(data)
 
 	-- determine if we should close nvim-tree here
 	-- If no other buffers are active, and none are modified, we close
-	local should_close = 1
---	local buffers = vim.fn.getbufinfo()
---	for i=1,#buffers,1 do
---		if buffers[i].hidden == 0 or buffers[i].changed == 1 then
---			should_close = 0
---			break
---		end
---	end
 	local windows = vim.fn.getwininfo()
-	local non_essential_count = 0 
+	local non_essential_count = 1 --currently closing window counts as 1
 	--print(#windows)
 	for i, win in pairs(windows) do
-		if vim.bo[vim.api.nvim_win_get_buf(win.winid)].filetype ~= "" then
+--		print("test " .. vim.bo[vim.api.nvim_win_get_buf(win.winid)].filetype)
+		if vim.bo[vim.api.nvim_win_get_buf(win.winid)].filetype == "noice" then
+			non_essential_count = non_essential_count + 1
+		end
+		if vim.bo[vim.api.nvim_win_get_buf(win.winid)].filetype == "NvimTree" then
 			non_essential_count = non_essential_count + 1
 		end
 	end
