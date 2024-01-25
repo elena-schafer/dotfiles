eca3d82795b5758ff23f7a554c2b9f47896eb5f5
Egan Schafer <eganschafer@gmail.com>
2024-01-25 16:06:29 -0700

    fix: improved buffer close keybind

--- nvim/lua/custom-keys.lua
+++ nvim/lua/custom-keys.lua
@@ -7,8 +7,15 @@ map('n', '<C-n>', [[:NvimTreeToggle<CR>]], {})
 map('n', '[b', [[:BufferLineCyclePrev<CR>]], {})
 map('n', ']b', [[:BufferLineCycleNext<CR>]], {})
 map('n', '<leader>bp', [[:BufferLineTogglePin<CR>]], {})
-map('n', '<leader>q', [[:e#<bar>bd #<CR>]], {}) --change to more complex function to allow closing nvim when all edit buffers are closed
+function closeBuffer () 
+	vim.api.nvim_buf_delete(0, {})
+	--local bufferline = require('bufferline')
+	--if (#bufferline.get_elements().elements == 1) then
+	--end
+end
+--map('n', '<leader>q', [[:e#<bar>bd #<CR>]], {}) --change to more complex function to allow closing nvim when all edit buffers are closed
 						--currently just reopens the alternative file
+vim.keymap.set('n', '<leader>q', closeBuffer, {})
 
 --floaterm keybinds
 map('n', '<leader>ft', [[:FloatermNew<CR>]], {})
