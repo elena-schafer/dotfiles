fa634c91dd9ac068995b80f3f1f02c41b7e44133
Egan Schafer <eganschafer@gmail.com>
2024-02-04 07:27:31 -0700

    feat: added keybinds for certain lsp features

--- nvim/lua/custom-keys.lua
+++ nvim/lua/custom-keys.lua
@@ -43,3 +43,26 @@ vim.keymap.set("n", "[t", function()
 end, { desc = "Previous todo comment" })
 map('n', '<leader>ft', [[:TodoTelescope<CR>]], {})
 
+--LSP keybinds
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local bufmap = function(mode, lhs, rhs)
+      local opts = {buffer = event.buf}
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- Display documentation of the symbol under the cursor
+    bufmap('n', '<leader>ld', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+    -- Jump to the definition
+    bufmap('n', '<leader>lg', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+    -- Renames all references to the symbol under the cursor
+    bufmap('n', '<leader>lr', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+    -- Selects a code action available at the current cursor position
+    bufmap('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+  end
+})
+
