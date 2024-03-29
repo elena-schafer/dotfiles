8110ae593c07e5956fdfbb677b3ddffd64c1187f
Egan Schafer <eganschafer@gmail.com>
2024-03-29 09:08:01 -0600

    fix: exit manager now works properly when notification is displayed

--- nvim/lua/exit-manager.lua
+++ nvim/lua/exit-manager.lua
@@ -4,10 +4,15 @@ local function close_nvim_tree_on_exit(data)
 	-- determine if we should close nvim-tree here
 	local windows = vim.fn.getwininfo()
 	local non_essential_count = 1 --currently closing window counts as 1
-	--print(#windows)
+
+
+	-- NOTE: this must be updated whenever some plugin or something adds a new window or else closing won't work smoothly
 	for i, win in pairs(windows) do
 		--print("test " .. vim.bo[vim.api.nvim_win_get_buf(win.winid)].filetype)
 		if vim.bo[vim.api.nvim_win_get_buf(win.winid)].filetype == "noice" then
+			non_essential_count = non_essential_count + 1
+		end
+		if vim.bo[vim.api.nvim_win_get_buf(win.winid)].filetype == "notify" then
 			non_essential_count = non_essential_count + 1
 		end
 		if vim.bo[vim.api.nvim_win_get_buf(win.winid)].filetype == "NvimTree" then
@@ -27,3 +32,11 @@ local function close_nvim_tree_on_exit(data)
 end
 
 vim.api.nvim_create_autocmd({ "QuitPre" }, { callback = close_nvim_tree_on_exit })
+
+--[[
+]]--
+function check_win_names()
+	for i, win in pairs(vim.fn.getwininfo()) do
+		print(vim.bo[vim.api.nvim_win_get_buf(win.winid)].filetype)
+	end
+end
