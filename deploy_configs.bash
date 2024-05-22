95cec5e12af9ea75dad7e3dfff4e1f8d1d5c2528
Egan Schafer <eganschafer@gmail.com>
2024-05-22 16:01:02 -0600

    Removed old install script

--- deploy_configs.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Deploy nvim configs to standard location
-ln -s $(realpath $(dirname $0))/nvim ~/.config/
-
-rm ~/.zshrc
-ln -s $(realpath $(dirname $0))/zsh/.zshrc ~/
-
-#echo $(realpath $(dirname $0))
