9e81d599e9b38d9ff71fc30829d54d85787df7d4
Egan Schafer <eganschafer@gmail.com>
2024-05-25 08:05:41 -0600

    Added motions based on treesitter nodes

--- nvim/.config/nvim/lua/sitter.lua
+++ nvim/.config/nvim/lua/sitter.lua
@@ -3,4 +3,40 @@ require("nvim-treesitter.configs").setup {
   highlight = { enable = true, },
   incremental_selection = { enable = true, },
   indent = { enable = true, },
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+      },
+      include_surrounding_whitespace = true,
+    },
+		move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]f"] = "@function.outer",
+        ["]c"] = { query = "@class.outer", desc = "Next class start" },
+      },
+      goto_next_end = {
+        ["]F"] = "@function.outer",
+        ["]C"] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[f"] = "@function.outer",
+        ["[c"] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[F"] = "@function.outer",
+        ["[C"] = "@class.outer",
+      },
+    },
+  },
 }
