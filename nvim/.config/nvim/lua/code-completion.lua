8b20f4e30b2573777fc128ed4cabc28d03b387f0
Egan Schafer <eganschafer@gmail.com>
2024-05-28 18:56:47 -0600

    Reworked completion keybinds

--- nvim/.config/nvim/lua/code-completion.lua
+++ nvim/.config/nvim/lua/code-completion.lua
@@ -11,21 +11,10 @@ cmp.setup({
   preselect = cmp.PreselectMode.None,
 
   mapping = {
-    -- Shift+TAB to go to the Previous Suggested item
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    -- Tab to go to the next suggestion
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    -- CTRL+SHIFT+f to scroll backwards in description
-    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
-    -- CTRL+F to scroll forwards in the description
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    -- CTRL+SPACE to bring up completion at current Cursor location
-    ['<C-Space>'] = cmp.mapping.complete(),
-    -- CTRL+e to exit suggestion and close it
-    ['<C-e>'] = cmp.mapping.close(),
-    -- CR (enter or return) to CONFIRM the currently selection suggestion
-    -- We set the ConfirmBehavior to insert the Selected suggestion
-    ['<CR>'] = cmp.mapping.confirm({
+    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<Leader>c'] = cmp.mapping.complete(),
+    ['<C-y>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     }),
