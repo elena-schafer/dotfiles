c8f79906f540f0943e67f1885d62c78b3b19963d
Egan Schafer <eganschafer@gmail.com>
2024-10-18 10:26:23 -0600

    Fixed lua lsp

--- nvim/.config/nvim/lua/lsp_setup.lua
+++ nvim/.config/nvim/lua/lsp_setup.lua
@@ -3,10 +3,11 @@ local util = require "lspconfig/util"
 
 lspconfig.clangd.setup{}
 lspconfig.lua_ls.setup({
+	cmd = { vim.fn.stdpath("data") .. "/mason/packages/lua-language-server/lua-language-server" },
 	on_init = function(client)
-	local path = client.workspace_folders[1].name
-	if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-	  return
+		local path = client.workspace_folders[1].name
+		if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+			return
 	end
 
 	client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
