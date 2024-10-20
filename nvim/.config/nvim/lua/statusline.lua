bd02cb4233e978684bf6b6300302fba0e40472b4
Egan Schafer <eganschafer@gmail.com>
2024-10-19 19:32:11 -0600

    Removed bufferline at top

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -2,12 +2,4 @@ require("lualine").setup{
 	options = {
 		theme = "catppuccin",
 	},
-	tabline = {
-		lualine_a = {},
-		lualine_b = {{'buffers', mode = 2}},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = {},
-	},
 }
