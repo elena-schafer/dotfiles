9bfb82ee25e9af96ea470e4838f312a15a6ec88c
Egan Schafer <eganschafer@gmail.com>
2025-01-06 09:31:26 -0700

    Added all config stows to install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 # Call this from inside dotfiles directory
 
-stow -D nvim
-stow -D zsh
-stow -D tmux
+stow -D ags
 stow -D alacritty
 stow -D greetd -t /
 stow -D hyfetch
+stow -D hyperland
+stow -D nvim
+stow -D rofi
+stow -D scripts
+stow -D tmux
+stow -D zsh
 
-stow nvim
-stow zsh
-stow tmux
+stow ags
 stow alacritty
 stow greetd -t /
 stow hyfetch
+stow hyperland
+stow nvim
+stow rofi
+stow scripts
+stow tmux
+stow zsh
