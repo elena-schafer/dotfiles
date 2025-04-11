3993dace2bbec8817f66a42ea18c0c70325d3ae3
Egan Schafer <eganschafer@gmail.com>
2025-04-11 06:14:35 -0600

    Sending aux tex files to /tmp if we are in the notes dir

--- nvim/.config/nvim/lua/config/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/config/plugins/vimtex.lua
@@ -7,6 +7,16 @@ return {
 			vim.g.vimtex_view_method = "zathura_simple"
 			vim.g.vimtex_quickfix_open_on_warning = 0
 			vim.g.vimtex_quickfix_ignore_filters = { 'Underfull \\hbox', 'Overfull \\hbox', }
+			vim.g.vimtex_compiler_latexmk = {
+				aux_dir = function(file_info)
+					-- Send auxiliary file to /tmp, to avoid us having to manually do cleanup
+					notes = vim.fn.expand("$HOME/notes/latex")
+					if (file_info.root == notes) then
+						return "/tmp/latex_notes_aux"
+					end
+					return file_info.root
+				end,
+			}
 		end
 	},
 }
