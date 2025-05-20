23db42e3d9853fde3688b9105fe3cfdbcaa4a944
Elena Schafer <elenaschafer12@gmail.com>
2025-05-20 16:22:56 -0500

    Added airplane mode indicator on status bar

--- ags/.config/ags/widgets/bar/wifi.lua
+++ ags/.config/ags/widgets/bar/wifi.lua
@@ -1,4 +1,5 @@
 local astal = require("astal")
+local Variable = astal.Variable
 local Widget = require("astal.gtk3.widget")
 local bind = astal.bind
 local Network = astal.require("AstalNetwork")
@@ -6,23 +7,31 @@ local Network = astal.require("AstalNetwork")
 return function()
 	local network = Network.get_default()
 	local wifi = bind(network, "wifi")
+	local w_icon = bind(wifi:get(), "icon-name")
+
+	local airplane = Variable(""):poll(1000, "bash -c \"rfkill -o SOFT | grep unblocked || true\"")
+	local icon = Variable.derive({ airplane, w_icon }, function(a, w)
+		if a == "" then
+			return "airplane-mode-symbolic"
+		elseif a == nil then
+			return "airplane-mode-symbolic"
+		else
+			return w
+		end
+	end)
 
 	return Widget.Box({
 		class_name = "wifi",
+		visible = wifi:as(function(v) return v ~= nil end),
 		Widget.Button({
 			on_clicked = function()
 				astal.exec("wifi-menu")
 			end,
-			visible = wifi:as(function(v) return v ~= nil end),
-			wifi:as(
-				function(w)
-					return Widget.Icon({
-						tooltip_text = bind(w, "ssid"):as(tostring),
-						class_name = "wifi",
-						icon = bind(w, "icon-name"),
-					})
-				end
-			),
+			Widget.Icon({
+				tooltip_text = bind(wifi, "ssid"):as(tostring),
+				class_name = "wifi",
+				icon = bind(icon):as(tostring)
+			})
 		})
 	})
 end
